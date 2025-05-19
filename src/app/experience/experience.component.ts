dd43a3f850e60374ccf28e6c97b57bc689e9f9a0
Atul <atul_2101ai08@iitp.ac.in>
2025-05-19 17:10:05 +0530

    profile update

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -13,10 +13,23 @@ export class ExperienceComponent {
 
   exps: any[] = [
     {
+      logo: 'assets/logo-sr.png',
+      role: 'Prism Intern @Samsung R&D Institute India',
+      start: 'June 2024',
+      end: 'March 2025',
+      line1:
+        'Developed a lightweight, on-device inference model for automated thumbnail generation from movies.',
+      line2:
+        'Designed an interactive module to regenerate thumbnails based on user feedback.',
+      line3:
+        'Optimized algorithms for real-time usability and scalability in video analysis.',
+      url: 'https://drive.google.com/file/d/1ixAKRcCYUlQ2t3Gv87CYxWisnbL4Pz1n/view?usp=sharing',
+    },
+    {
       logo: 'assets/logo.webp',
       role: 'Developer Intern @Fastcurve',
       start: 'June 2024',
-      end: 'Present',
+      end: 'September 2024',
       line1:
         'Developed and implemented AI-based Face Recognition Technology for the Enkept product.',
       line2:
@@ -29,7 +42,7 @@ export class ExperienceComponent {
       logo: 'assets/iitp_campus.webp',
       role: 'UG Researcher @IIT Patna',
       start: 'June 2023',
-      end: 'Present',
+      end: 'April 2025',
       line1:
         'Utilized efficient knowledge distillation methods to improve medical image analysis accuracy by 2-6%.',
       line2:
